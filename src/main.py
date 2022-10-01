1042377aa437c51a5d71bc0825a3d352f12ebe11
Marcus Bakke <marcusabakke@gmail.com>
2022-10-01 08:06:03 -0700

    Update drill sprite to have cardinal directions and animations

--- src/main.py
+++ src/main.py
@@ -4,13 +4,22 @@ from player import Player
 from sprite import Sprite
 
 def setup_image_bank():
-    pyxel.image(0).load(20, 20, "../assets/drill.png")
+    pyxel.image(0).load(0, 0, "../assets/drill.png")
 
 class App:
     def __init__(self):
-        pyxel.init(256, 256, title='Dig, Dig, Digger!')
+        pyxel.init(WINDOW_WIDTH, WINDOW_HEIGHT, title='Dig, Dig, Digger!')
         setup_image_bank()
-        self.player = Player(30, 10, Sprite(0, 20, 20, 16, 16))
+        self.player = Player(30, 10, [
+            Sprite(0, 0, 0, 16, 16),
+            # Sprite(0, 16, 0, 16, 16),
+            Sprite(0, 32, 0, 16, 16),
+            # Sprite(0, 48, 0, 16, 16),
+            Sprite(0, 64, 0, 16, 16),
+            # Sprite(0, 80, 0, 16, 16),
+            Sprite(0, 96, 0, 16, 16),
+            # Sprite(0, 112, 0, 16, 16),
+        ])
         pyxel.run(self.update, self.draw)
 
     def update(self):
--- src/player.py
+++ src/player.py
@@ -7,20 +7,30 @@ class Player(Thing):
         self.x = x
         self.y = y
         self.sprite = sprite
-        self.direction = 1
-        self.moveable = True
+        self.direction = 2
         self.speed = 1
 
     def update(self):
-        if self.moveable:
-            if pyxel.btnp(pyxel.KEY_LEFT):
-                self.x -= self.speed
-            if pyxel.btnp(pyxel.KEY_RIGHT):
-                self.x += self.speed
-            if pyxel.btnp(pyxel.KEY_UP):
-                self.y -= self.speed
-            if pyxel.btnp(pyxel.KEY_DOWN):
-                self.y += self.speed
+        if pyxel.btnp(pyxel.KEY_LEFT):
+            self.x -= self.speed
+            self.direction = 3
+        if pyxel.btnp(pyxel.KEY_RIGHT):
+            self.x += self.speed
+            self.direction = 1
+        if pyxel.btnp(pyxel.KEY_UP):
+            self.y -= self.speed
+            self.direction = 0
+        if pyxel.btnp(pyxel.KEY_DOWN):
+            self.y += self.speed
+            self.direction = 2
 
     def draw(self):
-        pyxel.blt(self.x, self.y, self.sprite.img, self.sprite.u, self.sprite.v, self.sprite.w, self.sprite.h)
+        pyxel.blt(
+            self.x,
+            self.y,
+            self.sprite[self.direction].img,
+            self.sprite[self.direction].u,
+            self.sprite[self.direction].v,
+            self.sprite[self.direction].w,
+            self.sprite[self.direction].h,
+            )
