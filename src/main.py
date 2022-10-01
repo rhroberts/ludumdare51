db70192bf350808e4872a667b87bc88b24631716
Cody <chorst22@gmail.com>
2022-09-30 21:49:14 -0700

    example

--- src/main.py
+++ src/main.py
@@ -1,9 +1,30 @@
 import pyxel
 
+
+class Player:
+    x = 32
+    y = 32
+
+    def __init__(self):
+        pass
+
+    def draw(self):
+        pyxel.blt(20, 20, 0, 20, 20, self.x, self.y)
+
+
+def setup_image_bank():
+    pyxel.image(0).load(20, 20, "../assets/sprites/drill.png")
+
+
 class App:
     def __init__(self):
         pyxel.init(160, 120)
         self.x = 0
+
+        setup_image_bank()
+        
+        self.player = Player()
+
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -12,5 +33,7 @@ class App:
     def draw(self):
         pyxel.cls(0)
         pyxel.rect(self.x, 0, 8, 8, 9)
+
+        self.player.draw()
 
 App()
