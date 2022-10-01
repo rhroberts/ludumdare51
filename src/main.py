19bddb64c3cd715ee9bbd3074d86554b6d0b9e1f
Marcus Bakke <marcusabakke@gmail.com>
2022-09-30 23:40:49 -0700

    Add Thing and Sprite classes. Update Player to inherit Thing

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pyxel
 from config import *
 from player import Player
+from sprite import Sprite
 
 def setup_image_bank():
     pyxel.image(0).load(20, 20, "../assets/drill.png")
@@ -9,10 +10,12 @@ class App:
     def __init__(self):
         pyxel.init(256, 256, title='Dig, Dig, Digger!')
         setup_image_bank()
-        self.player = Player(30, 10)
+        self.player = Player(30, 10, Sprite(0, 20, 20, 16, 16))
         pyxel.run(self.update, self.draw)
 
     def update(self):
+        if pyxel.btn(pyxel.KEY_Q):
+            pyxel.quit()
         self.player.update()
 
     def draw(self):
